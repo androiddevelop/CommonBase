1ba5b88ecd8261daefcbb7df6d2f8f93c1d83c01
玄恒 <yuedong.lyd@alibaba-inc.com>
2015-11-04 21:13:28 +0800

    抽象Http与Https相同部分

---
+++ src/main/java/me/codeboy/common/base/net/CBNetParam.java
@@ -0,0 +1,45 @@
+package me.codeboy.common.base.net;
+
+import java.util.Map;
+
+/**
+ * Created by yuedong on 11/3/15.
+ */
+public class CBNetParam {
+    /**
+     * convert parameters to string
+     *
+     * @param params
+     *         parameter
+     * @return 拼接后的字符串
+     */
+    public static String paramsToString(String... params) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < params.length - 1; i = i + 1) {
+            sb.append("&");
+            sb.append(params[i]);
+            sb.append("=");
+            sb.append(params[i + 1]);
+        }
+        return sb.substring(1);
+    }
+
+    /**
+     * convert parameters to string
+     *
+     * @param params
+     *         parameter
+     * @return 拼接后的字符串
+     */
+
+    public static String paramsToString(Map<String, String> params) {
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, String> item : params.entrySet()) {
+            sb.append("&");
+            sb.append(item.getKey());
+            sb.append("=");
+            sb.append(item.getValue());
+        }
+        return sb.substring(1);
+    }
+}
