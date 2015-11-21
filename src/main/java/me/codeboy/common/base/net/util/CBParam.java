4292e17ceed0c7bc61fb9ac41cb6c324a089b29c
玄恒 <yuedong.lyd@alibaba-inc.com>
2015-11-21 20:33:21 +0800

    完善参数转化操作

--- src/main/java/me/codeboy/common/base/net/util/CBParam.java
+++ src/main/java/me/codeboy/common/base/net/util/CBParam.java
@@ -1,5 +1,7 @@
 package me.codeboy.common.base.net.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 /**
@@ -10,17 +12,23 @@ public class CBParam {
     /**
      * convert parameters to string
      *
+     * @param charset
+     *         编码
      * @param params
      *         parameter
      * @return 拼接后的字符串
      */
-    public static String paramsToString(String... params) {
+    public static String paramsToString(String charset, String... params) {
         StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < params.length - 1; i = i + 1) {
-            sb.append("&");
-            sb.append(params[i]);
-            sb.append("=");
-            sb.append(params[i + 1]);
+        try {
+            for (int i = 0; i < params.length - 1; i = i + 1) {
+                sb.append("&");
+                sb.append(URLEncoder.encode(params[i], charset));
+                sb.append("=");
+                sb.append(URLEncoder.encode(params[i + 1], charset));
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
         return sb.substring(1);
     }
@@ -28,18 +36,24 @@ public class CBParam {
     /**
      * convert parameters to string
      *
+     * @param charset
+     *         编码
      * @param params
      *         parameter
      * @return 拼接后的字符串
      */
 
-    public static String paramsToString(Map<String, String> params) {
+    public static String paramsToString(String charset, Map<String, String> params) {
         StringBuffer sb = new StringBuffer();
-        for (Map.Entry<String, String> item : params.entrySet()) {
-            sb.append("&");
-            sb.append(item.getKey());
-            sb.append("=");
-            sb.append(item.getValue());
+        try {
+            for (Map.Entry<String, String> item : params.entrySet()) {
+                sb.append("&");
+                sb.append(URLEncoder.encode(item.getKey(), charset));
+                sb.append("=");
+                sb.append(URLEncoder.encode(item.getValue(), charset));
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
         return sb.substring(1);
     }
